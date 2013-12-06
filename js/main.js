e71a6a40835014437fe02715627ceeab59adbb5a
ozanalptekin <ozan.alptekin@gmail.com>
2013-12-06 13:09:29 +0100

    add parse markers

--- js/main.js
+++ js/main.js
@@ -1,18 +1,62 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
 
-  var CUSTOM_STYLES = !true;
+  var CUSTOM_STYLES = true;
   var lat = 59.3571220;
   var lon = 18.0552270;
+  var map;
+
+  var markers = [];
+
+  function getPatches() {
+
+    for (var i = 0, marker; marker = markers[i]; i++) {
+        marker.setMap(null);
+    }
+
+    var PatchObject = Parse.Object.extend("patch");
+    var query = new Parse.Query(PatchObject);
+    query.find({
+      success: function(results) {
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results.length; i++) { 
+          var owner    = results[i].get("owner");
+          var farmer   = results[i].get("farmer");
+          var location = results[i].get("location");
+          var process  = results[i].get("process");
+          var product  = results[i].get("product");
+
+          alert("PATCH: " + owner + ", " + farmer + ", " + location.latitude + ", " + location.longitude + ", " + process + ", " + product );
+          markers[i] =  results[i];
+
+          // To add the marker to the map, use the 'map' property
+
+
+        markers.push(marker);
+
+        var myLatlng = new google.maps.LatLng(lat,lon);
+        var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title:"Hello World!"
+        });
+        }
+      },
+      error: function(error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+  }
 
   function initialize() {
 
     if (CUSTOM_STYLES) {
 
+      /*
       var layer = "watercolor";
-      var map = new google.maps.Map(document.getElementById("map-canvas"), {
+      map = new google.maps.Map(document.getElementById("map-canvas"), {
         center: new google.maps.LatLng(lat, lon),
-        zoom: 12,
+        zoom: 15,
         mapTypeId: layer,
         mapTypeControlOptions: {
             mapTypeIds: [layer]
@@ -20,8 +64,14 @@
       });
 
       map.mapTypes.set(layer, new google.maps.StamenMapType(layer));
-
       console.log(layer);
+      */
+      var mapOptions = {
+        zoom: 15,
+        center: new google.maps.LatLng(59.3571220,18.0552270)
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      
 
     } else {
       // var kmlUrl = '/kml/dzialka.kml';
@@ -64,7 +114,7 @@
     // Create a map object, and include the MapTypeId to add
     // to the map type control.
     var mapOptions = {
-      zoom: 11,
+      zoom: 15,
       center: new google.maps.LatLng(lat, lon),
       mapTypeControlOptions: {
         mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
@@ -76,25 +126,6 @@
     //Associate the styled map with the MapTypeId and set it to display.
     map.mapTypes.set('map_style', styledMap);
     map.setMapTypeId('map_style');
-
-  }
-
-    // Init parse 
-    // Parse.initialize(wFUtsCkSqOff2CzUc1T5v75quD4kgy0hzenu6PQy, 
-    //                  4LnUhkWbhKNAs3N5rzGtgDoVz38NdxxhL6gPMbBg);
-      
-    // var TestObject = Parse.Object.extend("TestObject");
-    // var testObject = new TestObject();
-    //   testObject.save({foo: "bar"}, {
-    //   success: function(object) {
-    //     $(".success").show();
-    //   },
-    //   error: function(model, error) {
-    //     $(".error").show();
-    //   }
-    // });
-
-    var markers = [];
 
     // Create the search box and link it to the UI element.
     var input = /** @type {HTMLInputElement} */(
@@ -111,12 +142,7 @@
     google.maps.event.addListener(searchBox, 'places_changed', function() {
       var places = searchBox.getPlaces();
 
-      for (var i = 0, marker; marker = markers[i]; i++) {
-        marker.setMap(null);
-      }
-
       // For each place, get the icon, place name, and location.
-      markers = [];
       var bounds = new google.maps.LatLngBounds();
       for (var i = 0, place; place = places[i]; i++) {
         var image = {
@@ -127,22 +153,18 @@
           scaledSize: new google.maps.Size(25, 25)
         };
 
-        // Create a marker for each place.
-        var marker = new google.maps.Marker({
-          map: map,
-          icon: image,
-          title: place.name,
-          position: place.geometry.location
-        });
-
-        markers.push(marker);
-
         bounds.extend(place.geometry.location);
       }
 
       map.fitBounds(bounds);
     });
     // [END region_getplaces]
+
+  }
+
+    // Init parse 
+    Parse.initialize("wFUtsCkSqOff2CzUc1T5v75quD4kgy0hzenu6PQy", "4LnUhkWbhKNAs3N5rzGtgDoVz38NdxxhL6gPMbBg");
+    getPatches();
 
     // Bias the SearchBox results towards places that are within the bounds of the
     // current map's viewport.
